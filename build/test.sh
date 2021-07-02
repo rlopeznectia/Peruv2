b529bf5ae526aae04e267fd3be2c03a81fde43db
Karenrodriguezlab <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-07-02 14:01:13 -0400

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -2,9 +2,6 @@
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
 echo "Conectar Sodexo"
 sfdx force:org:list
-sfdx alias:set my-hub-orgUAT2=consultor_force@nectia.com.peru.prod.uat2
-sfdx force:apex:test:run -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
-#sfdx force:org:list
-#sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
-#sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
-#sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
+sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
+sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
+sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
