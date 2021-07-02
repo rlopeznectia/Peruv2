72ddeae707148bb79a0dd2715b60dba8633d5a78
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-07-02 13:01:14 -0400

    Carga Apex

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -2,4 +2,5 @@
 echo "Setting up DevHub Connection..."
 echo "Authenticating org"
 echo "Conectar UAT2"
+sfdx alias:set my-hub-orgUAT2=consultor_force@nectia.com.peru.prod.uat2
 sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,7 @@
 #sfdx force:auth:web:login -a $SFDC_UAT_USER -r https://test.salesforce.com/ -s
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
 echo "Conectar Sodexo"
-sfdx alias:set my-hub-orgUAT2=consultor_force@nectia.com.peru.prod.uat2
-sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
 sfdx force:org:list
-sfdx force:apex:test:run -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
+sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
+sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
+sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
