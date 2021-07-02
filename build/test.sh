5ecea0dac0ce0ae12ade991799b2d51af8fe1e74
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-07-02 17:00:56 -0400

    Carga Desarrollos

--- build/test.sh
+++ build/test.sh
@@ -4,4 +4,4 @@ sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
 sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
 sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
 sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod
-sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-Prod -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
+sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-Prod -w 10 -l RunLocalTests -w 33
