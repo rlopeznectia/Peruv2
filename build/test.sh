f828ec259d1edbca200f6e1054a5cb07db0cb2f5
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-07-05 14:38:47 -0400

    Carga Desarrollos

--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,8 @@
 echo "Conectar Sodexo"
 sfdx force:org:list
 sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
-sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
-sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
+#sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod.uat2
+#sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgUAT2 -w 10 -l RunLocalTests -w 33 --verbose --loglevel fatal
 sfdx config:set defaultusername=consultor_force@nectia.com.peru.prod
 sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-Prod -w 10 -l RunLocalTests -w 33
+sfdx force:mdapi:deploy:report
